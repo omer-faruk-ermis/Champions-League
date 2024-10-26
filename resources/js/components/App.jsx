0c3195db6cec15aa0a2c2cf6765b65665fb0c866
omerfarukermis <frybasgames@gmail.com>
2024-10-26 11:56:43 +0300

    Done React settings

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TeamList from "./TeamList.jsx";
+
+function App() {
+    return (
+        <div>
+            <h1>Insider Champions League</h1>
+            <TeamList />
+        </div>
+    );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.jsx', 'resources/css/app.css'],
+            refresh: true,
+        }),
+        react(),
+    ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
